31cf55296da0653c22e933002e03bf45af3df274
rachaelmob <rachael.mo.b@gmail.com>
2024-04-16 14:09:12 +0100

    Slide Out for smaller screens
    Working on Navigation for tablets and Ipads

--- js/sidenav.js
+++ js/sidenav.js
@@ -1,55 +1,17 @@
-const openBtn = document.querySelector('#openBtn');
-const closeBtn = document.querySelector('#closeBtn');
-const media = window.matchMedia('(width < $break--med)');
-const sideNavMenu = document.querySelector('.sidenav-menu');
-const pageContent = document.querySelector('#page-content');
-const body = document.querySelector('body');
+window.onload = function () {
+	window.addEventListener('scroll', function (e) {
+		if (window.scrollY > 100) {
+			document.querySelector("#sidenav").classList.add('is-scrolling');
+		} else {
+			document.querySelector("#sidenav").classList.remove('is-scrolling');
+		}
+	});
 
+	const menu_btn = document.querySelector('.hamburger');
+	const mobile_menu = document.querySelector('.mobile-nav');
 
-function setupSideNav(e) {
-    if (e.matches) {
-      // is mobile
-      console.log('is mobile');
-      sideNavMenu.setAttribute('inert', '');
-      sideNavMenu.style.transition = 'none';
-    } else {
-      // is tablet/desktop
-      console.log('is desktop');
-      closeMobileMenu();
-      sideNavMenu.removeAttribute('inert');
-    }
-  }
-
-
-function openMobileMenu(){
-    $('#openBtn').hide(); 
-    openBtn.setAttribute('aria-expanded', 'true');
-    sideNavMenu.removeAttribute('inert');
-    sideNavMenu.removeAttribute('style');
-    pageContent.setAttribute('inert', '');
-    bodyScrollLockUpgrade.disableBodyScroll(body);
-    closeBtn.focus();
+	menu_btn.addEventListener('click', function () {
+		menu_btn.classList.toggle('is-active');
+		mobile_menu.classList.toggle('is-active');
+	});
 }
-
-function closeMobileMenu(){
-    openBtn.setAttribute('aria-expanded', 'false');
-    sideNavMenu.setAttribute('inert', '');
-    pageContent.removeAttribute('inert');
-    bodyScrollLockUpgrade.enableBodyScroll(body);
-    openBtn.focus();
-  
-    setTimeout(() => {
-        sideNavMenu.style.transition = 'none';
-        // $('#openBtn').show(); //-> FAULTY
-      }, 500);
-
-}
-
-setupSideNav(media);
-
-openBtn.addEventListener('click', openMobileMenu);
-closeBtn.addEventListener('click', closeMobileMenu);
-
-media.addEventListener('change', function (e){
-    setupSideNav(e);
-});
